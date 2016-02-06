b8018d41023769867824e572a89c22ac742bbcf4
Andréas Anemyr <aa223ig@student.lnu.se>
2016-02-06 11:20:29 +0100

    register user possible now. It's time for authenticate with has_secure_password....

--- RegisterApplication/db/schema.rb
+++ RegisterApplication/db/schema.rb
@@ -14,10 +14,11 @@
 ActiveRecord::Schema.define(version: 20160205152156) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
