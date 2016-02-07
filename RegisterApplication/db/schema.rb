4a7e4a419c529685ea521e0bbbfb399fa152b6bd
Andréas Anemyr <aa223ig@student.lnu.se>
2016-02-07 09:51:50 +0100

    next - show logged in user in top - and button logout

--- RegisterApplication/db/schema.rb
+++ RegisterApplication/db/schema.rb
@@ -24,11 +24,12 @@ ActiveRecord::Schema.define(version: 20160206215339) do
   add_index "api_keys", ["user_id"], name: "index_api_keys_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "username",                        null: false
+    t.boolean  "admin",           default: false, null: false
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
--- RegisterApplication/db/seeds.rb
+++ RegisterApplication/db/seeds.rb
@@ -5,21 +5,30 @@
 #
   users = User.create (
                           [
-                              { username: 'Andreas' , email: 'andreas@anemyr.se' , password: 'secretpass' , password_confirmation: 'secretpass'},
-                              { username: 'Matilda' , email: 'matilda@anemyr.se' , password: 'secretpass' , password_confirmation: 'secretpass'},
-                              { username: 'Noa'     , email: 'noa@anemyr.se'     , password: 'secretpass' , password_confirmation: 'secretpass'},
-                              { username: 'Ellen'   , email: 'ellen@anemyr.se'   , password: 'secretpass' , password_confirmation: 'secretpass'}
+                              { username: 'Andreas' , email: 'andreas@anemyr.se' , password: 'secretpass' , password_confirmation: 'secretpass', admin: true },
+                              { username: 'Matilda' , email: 'matilda@anemyr.se' , password: 'secretpass' , password_confirmation: 'secretpass' },
+                              { username: 'Noa'     , email: 'noa@anemyr.se'     , password: 'secretpass' , password_confirmation: 'secretpass' },
+                              { username: 'Ellen'   , email: 'ellen@anemyr.se'   , password: 'secretpass' , password_confirmation: 'secretpass' },
+                              { username: 'Elia'    , email: 'elia@anemyr.se'    , password: 'secretpass' , password_confirmation: 'secretpass' },
+                              { username: 'Maja'    , email: 'maja@anemyr.se'    , password: 'secretpass' , password_confirmation: 'secretpass' },
+                              { username: 'Wilma'   , email: 'wilma@anemyr.se'   , password: 'secretpass' , password_confirmation: 'secretpass' }
                           ]
                       )
 
    api_keys = ApiKey.create (
                                 [
-                                    { key: SecureRandom.base64 , user_id: 0 , app: 'MyShop 1' },
-                                    { key: SecureRandom.base64 , user_id: 3 , app: 'MyShop 2' },
-                                    { key: SecureRandom.base64 , user_id: 0 , app: 'MyShop 3' },
-                                    { key: SecureRandom.base64 , user_id: 3 , app: 'MyShop 4' },
-                                    { key: SecureRandom.base64 , user_id: 2 , app: 'MyShop 5' },
-                                    { key: SecureRandom.base64 , user_id: 1 , app: 'MyShop 6' },
-                                    { key: SecureRandom.base64 , user_id: 1 , app: 'MyShop 7' }
+                                    { key: SecureRandom.base64 , user_id: 1 , app: 'MyShop 1' },
+                                    { key: SecureRandom.base64 , user_id: 2 , app: 'MyShop 2' },
+                                    { key: SecureRandom.base64 , user_id: 3 , app: 'MyShop 3' },
+                                    { key: SecureRandom.base64 , user_id: 4 , app: 'MyShop 4' },
+                                    { key: SecureRandom.base64 , user_id: 5 , app: 'MyShop 5' },
+                                    { key: SecureRandom.base64 , user_id: 6 , app: 'MyShop 6' },
+                                    { key: SecureRandom.base64 , user_id: 0 , app: 'MyHackedFacebook 1' },
+                                    { key: SecureRandom.base64 , user_id: 1 , app: 'MyHackedFacebook 2' },
+                                    { key: SecureRandom.base64 , user_id: 2 , app: 'MyHackedFacebook 3' },
+                                    { key: SecureRandom.base64 , user_id: 3 , app: 'MyHackedFacebook 4' },
+                                    { key: SecureRandom.base64 , user_id: 4 , app: 'MyHackedFacebook 5' },
+                                    { key: SecureRandom.base64 , user_id: 5 , app: 'MyHackedFacebook 6' },
+                                    { key: SecureRandom.base64 , user_id: 6 , app: 'MyHackedFacebook 7' }
                                 ]
                             )
